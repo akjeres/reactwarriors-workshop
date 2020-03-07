22b15daa212285a4a84f683e8440c58797081dae
akjeres <akjeres@gmail.com>
2020-03-07 12:28:29 +0200

    Day 8. queryStringParams

--- src/components/MovieTabs.jsx
+++ src/components/MovieTabs.jsx
@@ -2,44 +2,31 @@ import React from 'react';
 import SortButton from './SortButton';
 
 export default class MovieTabs extends React.Component {
+    getClassName = (example, value) => {
+        return example === value ? 'active' : null;
+    };
+
     render() {
-        const { sort, updateSort } = this.props,
-              switcher = 'active',
-              extra_className = {
-                  popularity_desc: null,
-                  revenue_desc: null,
-                  vote_average_desc: null,
-              };
-        switch (sort) {
-            case 'revenue.desc':
-                extra_className.revenue_desc = switcher;
-                break;
-            case 'vote_average.desc':
-                extra_className.vote_average_desc = switcher;
-                break;
-            default:
-                extra_className.popularity_desc = switcher;
-                break;
-        };
+        const { sort, updateSort } = this.props;
         return(
           <ul className="tabs nav nav-pills">
               <li className="nav-item">
                   <SortButton
-                      className={extra_className.popularity_desc}
+                      className={this.getClassName(sort, 'popularity.desc')}
                       handler={updateSort.bind(null, 'popularity.desc')}
                       text="Popularity desc"
                   />
               </li>
               <li className="nav-item">
                   <SortButton
-                      className={extra_className.revenue_desc}
+                      className={this.getClassName(sort, 'revenue.desc')}
                       handler={updateSort.bind(null, 'revenue.desc')}
                       text="Revenue desc"
                   />
               </li>
               <li className="nav-item">
                   <SortButton
-                      className={extra_className.vote_average_desc}
+                      className={this.getClassName(sort, 'vote_average.desc')}
                       handler={updateSort.bind(null, 'vote_average.desc')}
                       text="Vote average desc"
                   />
