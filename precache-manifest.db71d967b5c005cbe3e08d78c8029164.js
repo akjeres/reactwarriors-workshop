a211710fdaba2a741912dc3218e498a6552085b1
akjeres <akjeres@gmail.com>
2020-03-16 22:58:20 +0200

    Updates

---
+++ precache-manifest.db71d967b5c005cbe3e08d78c8029164.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "384fd2dc5c2a2d09a29b920603264833",
+    "url": "/reactwarriors-workshop/index.html"
+  },
+  {
+    "revision": "1bdb0072bbfa99489c12",
+    "url": "/reactwarriors-workshop/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "f2100f66cbcaf5622524",
+    "url": "/reactwarriors-workshop/static/css/main.5d937b94.chunk.css"
+  },
+  {
+    "revision": "1bdb0072bbfa99489c12",
+    "url": "/reactwarriors-workshop/static/js/2.8b3c27ad.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/reactwarriors-workshop/static/js/2.8b3c27ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2100f66cbcaf5622524",
+    "url": "/reactwarriors-workshop/static/js/main.12eecd49.chunk.js"
+  },
+  {
+    "revision": "d7e25a7be26e22c2c944",
+    "url": "/reactwarriors-workshop/static/js/runtime-main.2a191a5d.js"
+  }
+]);
